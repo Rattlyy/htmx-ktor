37424212b39949c03da65a1376fba075dee3d1e7
Rattlyy <giovannimariamazzone@gmail.com>
2023-08-23 21:13:35 +0200

    postgres

--- src/main/kotlin/it/rattly/plugins/db/Databases.kt
+++ src/main/kotlin/it/rattly/plugins/db/Databases.kt
@@ -10,7 +10,7 @@ import java.sql.*
 import kotlinx.coroutines.*
 
 fun Application.configureDatabases() {
-    val dbConnection: Connection = connectToPostgres(embedded = true)
+    val dbConnection: Connection = connectToPostgres(System.getenv("POSTGRES") != null)
 
     todoRoutes(dbConnection)
 }
@@ -42,10 +42,6 @@ fun Application.connectToPostgres(embedded: Boolean): Connection {
     return if (embedded) {
         DriverManager.getConnection("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", "root", "")
     } else {
-        val url = environment.config.property("postgres.url").getString()
-        val user = environment.config.property("postgres.user").getString()
-        val password = environment.config.property("postgres.password").getString()
-
-        DriverManager.getConnection(url, user, password)
+        DriverManager.getConnection("jdbc:${System.getenv("POSTGRES")}")
     }
 }
