02f07c640c2b65a5380aa1fc1443742387015d2f
Rattly <40183121+Rattlyy@users.noreply.github.com>
2024-02-13 17:24:21 +0100

    enable embedded db (my free render quota expired)

--- src/main/kotlin/it/rattly/plugins/db/Databases.kt
+++ src/main/kotlin/it/rattly/plugins/db/Databases.kt
@@ -14,7 +14,7 @@ import java.sql.*
 
 
 fun Application.configureDatabases() {
-    todoRoutes(TodoService(connectToPostgres(environment.developmentMode)))
+    todoRoutes(TodoService(connectToPostgres(System.getenv("USE_EMBEDDED_DB").toBoolean())))
 }
 
 /**
--- src/main/kotlin/it/rattly/views/todos/todo.list.kt
+++ src/main/kotlin/it/rattly/views/todos/todo.list.kt
@@ -10,6 +10,7 @@ fun HTML.listTodos() = layout {
     val uid = UUID.randomUUID()
 
     h1 { +"todos!!!" }
+    h2 { +"PS: Database is embedded, so it wipes every time the instance gets restarted by Render (after 5m of inactivity)"}
     div {
         attributes["hx-ext"] = "sse"
         attributes["sse-connect"] = "/sse?uid=$uid"
